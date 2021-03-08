058be17d75f12e04a4e3c796beaa86e66634ec35
Leonardo <you@example.com>
2021-03-08 13:49:07 -0300

    final commit

--- binary.js
+++ binary.js
@@ -6,7 +6,7 @@ let binaryTest;
 // função que checa se o valor é binário
 function isArrayBool(array) {
   for (var i of array) {
-    if (i != 0 && i != 1) return false;
+    if (i !== "0" && i !== "1") return false;
   }
   return true;
 }
